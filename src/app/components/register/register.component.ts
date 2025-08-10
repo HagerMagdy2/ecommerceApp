25a88d299488291b5fae4e1258008bb11917327a
HagerMagdy2 <hager9671@gmail.com>
2025-08-10 23:04:40 +0300

    updates

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,21 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule], 
+  imports: [ReactiveFormsModule,JsonPipe], 
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+UserRegisterForm:FormGroup;
+constructor(){
+  this.UserRegisterForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),//initial value
+  })
+}
 }
